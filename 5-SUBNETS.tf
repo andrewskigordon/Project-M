b229ea04eb718cf17f935a2176aa9f246d08b8b0
Andrew <andrewskigordon>
2024-04-05 16:50:40 -0400

    duplicate values need to be fixed

--- 5-SUBNETS.tf
+++ 5-SUBNETS.tf
@@ -10,20 +10,37 @@ resource "aws_subnet" "public-us-east-1b" {
 
 resource "aws_subnet" "public-us-east-1c" {
   vpc_id     = aws_vpc.Project-M-VPC.id
-  cidr_block = "10.74.3.0/24"
+  cidr_block = " 10.74.3.0/24"
 }
 
-resource "aws_subnet" "private-us-east-1a" {
+resource "aws_subnet" "Private-us-east-1a" {
   vpc_id     = aws_vpc.Project-M-VPC.id
   cidr_block = "10.74.11.0/24"
 }
 
-resource "aws_subnet" "private-us-east-1b" {
+resource "aws_subnet" "Private-us-east-1b" {
   vpc_id     = aws_vpc.Project-M-VPC.id
   cidr_block = "10.74.12.0/24"
 }
 
-resource "aws_subnet" "private-us-east-1c" {
+resource "aws_subnet" "Private-us-east-1c" {
   vpc_id     = aws_vpc.Project-M-VPC.id
   cidr_block = "10.74.13.0/24"
 }
+
+resource "aws_subnet" "Private-us-east-1a " {
+  vpc_id     = aws_vpc.Project-M-VPC.id
+  cidr_block = "10.74.21.0/24"
+}
+
+resource "aws_subnet" "Private-us-east-1b" {
+  vpc_id     = aws_vpc.Project-M-VPC.id
+  cidr_block = "10.74.22.0/24"
+}
+
+resource "aws_subnet" "Private-us-east-1c" {
+  vpc_id     = aws_vpc.Project-M-VPC.id
+  cidr_block = "10.74.23.0/24"
+}
+
+
